2c79dc1cc85814770a5a5c1e07ae7023fce49760
tanninaham <hamizi.tannina@gmail.com>
2024-03-06 16:32:09 +0100

    update

--- scripts/pretraitement.py
+++ scripts/pretraitement.py
@@ -4,11 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 # Charger le fichier CSV
-csv_path = '/Users/gemmafelton/Desktop/Interface_web/corpus/detection_dh.csv'
+csv_path = '/Users/tannina/Desktop/Interface_web/corpus/detection_dh.csv'
 df = pd.read_csv(csv_path)
 
 # Remplacer les valeurs nulles par une chaîne vide
-df['Text'] = df['Text'].fillna('')
+df['tweet'] = df['tweet'].fillna('')
 
 # Prétraitement des données
 def preprocess_text(text):
@@ -22,10 +22,10 @@ def preprocess_text(text):
     return text
 
 # Appliquer la fonction de prétraitement au texte
-df['Text'] = df['Text'].apply(preprocess_text)
+df['tweet'] = df['tweet'].apply(preprocess_text)
 
 # Sauvegarder le DataFrame modifié dans un nouveau fichier CSV
-new_csv_path = '/Users/gemmafelton/Desktop/Interface_web/corpus/detection_dh.csv'
+new_csv_path = '/Users/tannina/Desktop/Interface_web/corpus/detection_dh.csv'
 df.to_csv(new_csv_path, index=False)
 
 # Afficher les données après le prétraitement
@@ -33,8 +33,9 @@ print(df.head())
 
 # Diviser les données en ensembles d'entraînement et de test
 train_data, test_data, train_labels, test_labels = train_test_split(
-    df['Text'], df['oh_label'], test_size=0.2, random_state=42
+    df['tweet'], df['class'], test_size=0.2, random_state=42
 )
+
 
 # Mélanger les ensembles d'entraînement
 train_data, train_labels = shuffle(train_data, train_labels, random_state=42)
